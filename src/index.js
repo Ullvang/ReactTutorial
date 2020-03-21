a240596b164fdf11ed3b251f6d7c875005853afa
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2020-03-22 00:15:28 +0100

    Tried out elements in index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// const element = React.createElement("h1", null, "Welcome to React Programming");
+
+// ReactDOM.render(element, document.getElementById("root"));
+
+const element = React.createElement(
+  "div",
+  { className: "testClass" },
+  React.createElement("h1", null, "Welcome to React"),
+  React.createElement("h1", null, "Understanding the Creation of Elements")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(element, document.getElementById("root"));
+
+// const element = <h1 className="testClass">Welcome to React Programming..</h1>;
+// ReactDOM.render(element, document.getElementById("root"));
+
+// const newElement = (
+//   <h1 className="testClass">Understanding the Creation of Elements</h1>
+// );
+// ReactDOM.render(newElement, document.getElementById("root"));
+
+// const element = (
+//   <div className="testClass">
+//     <h1>Welcome to React Programming...</h1>
+//     <h1>Understanding the Creation of React Elements...</h1>
+//   </div>
+// );
+
+// ReactDOM.render(element, document.getElementById("root"));
